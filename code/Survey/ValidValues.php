812017840c9f341de246bc0ad280897eeb11ec5d
Tomáš Fedor <tomas.fedor3@gmail.com>
2022-01-24 03:54:26 +0100

    Revert "Merge pull request #3 from MxtOUT/hw-surveys"
    This reverts commit 3397a4eaf847684e08554ab3ef3a27885f89216d, reversing
    changes made to 5db77e776c3230e4160873582e9079780c284d74.

--- code/Survey/ValidValues.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("SURVEY_VALID_VALUES", [
-    "framerate" => ["th", "sx", "va"],
-    "optimized" => ["yes", "no"],
-    "lag" => ["yes", "no"],
-    "graphics_settings" => ["no", "bs", "gr"],
-    "bg_sound" => ["yes", "no"],
-    "good_controls" => ["yes", "no"],
-]);
